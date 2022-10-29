b502c3231485d1e5506e1cef8ea2b3d7e78f4541
Lorenz Miguel A. Gracia <84924193+lorenz-MotionGraphics@users.noreply.github.com>
2022-10-29 15:37:12 +0800

    Create cpp.cpp
    my first cpp project programming

---
+++ cpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <cstring>
+#include <cmath>
+#include <windows.h>
+
+int main () {
+    cout<<"hello world";  //hello world message
+  return 0;
+}
+
